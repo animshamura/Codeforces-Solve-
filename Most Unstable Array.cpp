cad89fb867fa9bb8cc2303b08a7363c62333492b
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-05-21 14:04:10 +0600

    Create Most Unstable Array.cpp

---
+++ Most Unstable Array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+  int t, n, m;
+  cin>>t;
+  while(t--){
+    cin>>n>>m;
+    cout<<min(2,n-1)*m<<endl;
+  }
+  return 0;
+}
