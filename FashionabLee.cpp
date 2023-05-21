6099cae9d8881145311e5c88478bbb6d88b78bfe
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-05-21 14:13:40 +0600

    Create FashionabLee.cpp

---
+++ FashionabLee.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int t,x;
+  cin>>t;
+  while(t--){
+    cin>>x;
+    if(x%4==0) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+  }
+  return 0;
+}
